f7e9985f9400a1569ec25101412a654fd4ba70fc
Obaid <ephiram2002@gmail.com>
2022-04-20 18:59:57 +0530

    Deployed On Heroku

--- MP/settings.py
+++ MP/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x0k-a!v0jwds_@=zj^1*aiubla*k#r%9%8q!49t3mk0&t^6tf0'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eda-simplifier.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
